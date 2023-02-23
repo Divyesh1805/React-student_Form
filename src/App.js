754e79cadb07379769559b89a32ac861a8622f19
Divyesh Gangwal <divyeshgangwal@gmail.com>
2023-02-23 22:53:42 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function StudentForm() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("")
+  const [confirm_password, setConfirm_password] = useState("")
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ name, age, gender, email });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="age">Age:</label>
+        <input
+          type="text"
+          id="age"
+          value={age}
+          onChange={(event) => setAge(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="gender">Gender:</label>
+        <select id="gender" value={gender} onChange={(event) => setGender(event.target.value)}>
+          <option value="">Please select</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+      </div>
+      <div>
+        <label htmlFOr="confirm_password">Confirm_password:</label>
+        <input
+          type="text"
+          id="password"
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default App;
+export default StudentForm;
